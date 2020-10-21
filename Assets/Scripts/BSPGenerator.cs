eeedad8e1c3e8c1f238d784ef3441356ffefe486
Emilia <42769841+emiliaszynkowska@users.noreply.github.com>
2020-10-21 11:39:09 +0100

    Generating Rooms
    to do: generate corridors

--- Assets/Scripts/BSPGenerator.cs
+++ Assets/Scripts/BSPGenerator.cs
@@ -1,18 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.CodeDom.Compiler;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BSPGenerator : MonoBehaviour
 {
+    //Tiles
+    public Tile groundTile;
+    public Tile wallTile;
+    public Tile edgeTile;
+    public Tile leftEdgeTile;
+    public Tile rightEdgeTile;
+    public Tile leftCornerTile;
+    public Tile rightCornerTile;
+    public Tile leftPointTile;
+    public Tile rightPointTile;
+    public Tile leftWallTile;
+    public Tile rightWallTile;
+    //Tilemaps
+    public Tilemap groundMap;
+    public Tilemap outerWallMap;
+    public Tilemap innerWallMap;
+    public Tilemap edgeMap;
+    //Variables
+    public int width;
+    public int height;
+    public int minRoomSize;
+    public int maxRoomSize;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Dungeon root = new Dungeon(new Rect(0,0,width,height));
+        Generate(root);
+        root.CreateRoom();
+        FillRooms(root);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Generate(Dungeon dungeon)
     {
-        
+        if (dungeon.IsLeaf())
+        {
+            if (dungeon.rect.width > maxRoomSize || dungeon.rect.height > maxRoomSize || Random.Range(0, 1) > 0.25)
+            {
+                if (dungeon.Split(minRoomSize, maxRoomSize))
+                {
+                    Generate(dungeon.left);
+                    Generate(dungeon.right);
+                }
+            }
+        }
     }
+
+    void FillRooms(Dungeon dungeon)
+    {
+        if (dungeon == null)
+            return;
+        if (dungeon.IsLeaf())
+        {
+            for (int x = (int) dungeon.room.x; x < dungeon.room.xMax; x++) {
+                for (int y = (int) dungeon.room.y; y < dungeon.room.yMax; y++) {
+                    groundMap.SetTile(new Vector3Int(x,y,0), groundTile);
+                }
+            }
+        } else {
+            FillRooms(dungeon.left);
+            FillRooms(dungeon.right);
+        }
+    }
+
 }
---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Dungeon
+    {
+        public Dungeon left;
+        public Dungeon right;
+        public Rect rect;
+        public Rect room;
+
+        public Dungeon(Rect rect)
+        {
+            this.rect = rect;
+        }
+
+        public bool IsLeaf()
+        {
+            return left == null && right == null; 
+        }
+
+        public bool Split(int minRoomSize, int maxRoomSize)
+        {
+            if (!IsLeaf())
+                return false;
+            
+            bool splitH;
+            if (rect.width / rect.height >= 1.25)
+                splitH = false;
+            else if (rect.height / rect.width >= 1.25)
+                splitH = true;
+            else
+                splitH = Random.Range(0, 1) > 0.5;
+
+            if (Math.Min(rect.height, rect.width) / 2 < minRoomSize)
+                return false;
+
+            if (splitH)
+            {
+                int split = Random.Range(minRoomSize, (int) (rect.width - minRoomSize));
+                left = new Dungeon(new Rect(rect.x, rect.y, rect.width, split));
+                right = new Dungeon(new Rect(rect.x, rect.y + split, rect.width, rect.height - split));
+            }
+            else
+            {
+                int split = Random.Range(minRoomSize, (int) (rect.height - minRoomSize));
+                left = new Dungeon(new Rect (rect.x, rect.y, split, rect.height));
+                right = new Dungeon(new Rect (rect.x + split, rect.y, rect.width - split, rect.height));
+            }
+
+            return true;
+        }
+
+        public void CreateRoom()
+        {
+            if (left != null)
+                left.CreateRoom();
+            if (right != null)
+                right.CreateRoom();
+            if (IsLeaf())
+            {
+                int width = (int) Random.Range(rect.width / 2, rect.width - 2);
+                int height = (int) Random.Range(rect.height / 2, rect.height - 2);
+                int roomX = (int) Random.Range(1, rect.width - width - 1);
+                int roomY = (int) Random.Range(1, rect.height - height - 1);
+                
+                room = new Rect(rect.x + roomX, rect.y + roomY, width, height);
+            }
+        }
+
+    }
